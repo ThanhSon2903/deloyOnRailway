20bc32543b866dcbd8450e774f31e5e3d1a622b2
ThanhSon2903 <thanhnguyensonjqk@gmail.com>
2026-07-10 13:25:21 +0700

    test bug send email

--- demo/src/main/java/DATN/example/demo/service/EmailOtpService.java
+++ demo/src/main/java/DATN/example/demo/service/EmailOtpService.java
@@ -15,15 +15,25 @@ public class EmailOtpService {
     JavaMailSender javaMailSender;
 
     public void sentOtpEmail(String toEmail,String otp){
-        try{
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(toEmail);
-            message.setSubject("Vui lòng xác thực tài khoản của bạn, mã OTP có hiệu lực 1 phút");
-            message.setText("Mã OTP của bạn là: " + otp);
-            javaMailSender.send(message);
-        }
-        catch (RuntimeException e) {
-            throw new RuntimeException(e);
-        }
+//        try{
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setTo(toEmail);
+//            message.setSubject("Vui lòng xác thực tài khoản của bạn, mã OTP có hiệu lực 1 phút");
+//            message.setText("Mã OTP của bạn là: " + otp);
+//            javaMailSender.send(message);
+//        }
+//        catch (RuntimeException e) {
+//            throw new RuntimeException(e);
+//        }
+        System.out.println("1. Chuẩn bị gửi mail");
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Vui lòng xác thực tài khoản");
+        message.setText("OTP: " + otp);
+
+        System.out.println("2. Trước send()");
+        javaMailSender.send(message);
+        System.out.println("3. Sau send()");
     }
 }
